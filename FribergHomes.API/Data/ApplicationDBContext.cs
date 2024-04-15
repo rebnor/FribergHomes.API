2ad0b8528049fd28b4ceb93fd59ca9906d07e77f
rebecka nordqvist <rebecka.nordqvist@hotmail.com>
2024-04-15 11:56:02 +0200

    DBContext

---
+++ FribergHomes.API/Data/ApplicationDBContext.cs
@@ -0,0 +1,27 @@
+﻿using FribergHomes.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FribergHomes.API.Data
+{
+    /* DataBase
+     * Name: FribergHomesDB
+     * @ Author : Rebecka 2024-04-15
+     */
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        DbSet<Category> Categories { get; set; }
+        DbSet<County> Counties { get; set; }
+        DbSet<Realtor> Realtors { get; set; }
+        DbSet<Agency> Agencies { get; set; }
+
+        // Old code
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FribergHomesDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        //}
+    }
+}
