fbad5d1dc9425f80a501b7cf413b8e5abdf16451
Tobias Ledin <tobias.ledin@gmail.com>
2024-04-15 13:12:26 +0200

    Created Apartment class (inherits from SalesObject).

---
+++ FribergHomes.API/Models/Apartment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FribergHomes.API.Models
+{
+    //Creator: Tobias Ledin
+
+    public class Apartment : SalesObject
+    {
+        [Required]
+        public int Level { get; set; }
+
+        [Required]
+        public bool Lift { get; set; }
+
+        protected Apartment(int price) : base(price)
+        {
+
+        }
+    }
+}
