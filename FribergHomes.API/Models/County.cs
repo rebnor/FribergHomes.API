bd7f97d267079f80939f24ea490ddce8f76ee040
Tobias Ledin <tobias.ledin@gmail.com>
2024-04-15 10:37:24 +0200

    Added County model.

---
+++ FribergHomes.API/Models/County.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FribergHomes.API.Models
+{
+
+    //Creator: Tobias Ledin
+
+    public class County
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Kommun")]
+        [Length(3, 20, ErrorMessage = "Kommunnamnet måste bestå av minst 3 och max 20 bokstäver")]
+        [RegularExpression(@"^[a-zA-ZåäöÅÄÖ\s-]+$", ErrorMessage = "Kommunnamnet får enbart innehålla a-ö, bindestreck och mellanslag")]
+        public required string Name { get; set; }
+
+    }
+}
