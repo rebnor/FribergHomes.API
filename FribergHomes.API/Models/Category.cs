e696d8afd4ce15633cb35e8bd62dafb552e1b958
rebecka nordqvist <rebecka.nordqvist@hotmail.com>
2024-04-15 10:21:40 +0200

    Category-Model

---
+++ FribergHomes.API/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace FribergHomes.API.Models
+{
+    /* Class for Category.
+     * IconUrl should be a https-link of an Icon, example FontAwesome.
+     * @ Author: Rebecka 2024-04-15
+     */
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconUrl { get; set; }
+
+    }
+}
