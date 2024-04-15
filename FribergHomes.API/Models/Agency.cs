7e2ffcae32d2225300e26424c2731e6a285f399f
sanna <sanna.nyberg@hotmail.se>
2024-04-15 10:38:15 +0200

    Model för Agency och Realtor har lagts till

---
+++ FribergHomes.API/Models/Agency.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FribergHomes.API.Models
+{
+    // Author: Sanna 
+    public class Agency
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Presentation { get; set; }
+        [Required]
+        public string Logo { get; set; }
+       
+        public List<Realtor>? Realtors { get; set; }
+    }
+}
