122f52f0b75318d8bfc7d3e9e7b8aafd75970d85
Tobias Ledin <tobias.ledin@gmail.com>
2024-04-15 17:03:26 +0200

    Created IAgency interface to be implemented by AgencyRepository.

---
+++ FribergHomes.API/Data/Interfaces/IAgency.cs
@@ -0,0 +1,18 @@
+﻿using FribergHomes.API.Models;
+
+namespace FribergHomes.API.Data.Interfaces
+{
+    /* Class for SalesObject which forms the base class for all real estate objects.
+      * Author: Tobias 2024-04-15
+      */
+
+    public interface IAgency
+    {
+        Task<Agency> GetAgencyByIdAsync(int id);
+        Task<List<Agency>> GetAllAgenciesAsync();
+        Task AddAgencyAsync(Agency agency);
+        Task<Agency> UpdateAgencyAsync(Agency agency);
+        Task DeleteAgencyAsync(int id);
+
+    }
+}
