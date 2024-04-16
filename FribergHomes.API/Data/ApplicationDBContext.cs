40e54bf847c79f231f125a54e367f0cb97e869b7
rebecka nordqvist <rebecka.nordqvist@hotmail.com>
2024-04-16 12:25:16 +0200

    Update DBContext to ConnectionString in appsettings.json

--- FribergHomes.API/Data/ApplicationDBContext.cs
+++ FribergHomes.API/Data/ApplicationDBContext.cs
@@ -1,16 +1,26 @@
 ﻿using FribergHomes.API.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace FribergHomes.API.Data
 {
     /* DataBase
      * Name: FribergHomesDB
      * @ Author : Rebecka 2024-04-15
+     * @ Update: Using configuration to get connectionstring from appsettings.json / Rebecka 2024-04-16
      */
     public class ApplicationDBContext : DbContext
     {
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        // Old code
+        //public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        //{
+        //}
+
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options, IConfiguration configuration) : base(options)
         {
+            _configuration = configuration;
         }
 
         public DbSet<Category> Categories { get; set; }
@@ -23,10 +33,11 @@ namespace FribergHomes.API.Data
         DbSet<Apartment> Apartments { get; set; }
          */
 
-        // Old code
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FribergHomesDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
-        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FribergHomesDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            string? connectionString = _configuration.GetConnectionString("FribergHomesDB");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
--- FribergHomes.API/Program.cs
+++ FribergHomes.API/Program.cs
@@ -18,29 +18,31 @@ namespace FribergHomes.API
              * First it will look for a Database called FribergHomesDB,
              * if not found a exception will be thrown and a Database will get created.
              * @ Author: Rebecka 2024-04-15
+             * @ Update: The code i thought about didnt work smooth, so i changed to get use sqlserver -> connectionstring. / Rebecka 2024-04-16
              */
-            builder.Services.AddDbContext<ApplicationDBContext>(options =>
-            {
-                // Looking for Database FribergHomesDB
-                var connectionString = builder.Configuration.GetConnectionString("FribergHomesDB");
-                //if not found
-                if (connectionString == null)
-                {
-                    throw new InvalidOperationException("Connection string 'FribergHomesDB' not found.");
-                }
-                // If found, the connection starts
-                options.UseSqlServer(connectionString);
-
-                // If no database called FribergHomesDB is found, it will create a Database with that name.
-                var dbContextOptions = new DbContextOptionsBuilder<ApplicationDBContext>().UseSqlServer(connectionString).Options;
-                using (var context = new ApplicationDBContext(dbContextOptions))
-                {
-                    context.Database.EnsureCreated();
-                }
-            });
-            // Old code
+            // Old code:
             //builder.Services.AddDbContext<ApplicationDBContext>(options =>
-            //    options.UseSqlServer(builder.Configuration.GetConnectionString("FribergHomesDB") ?? throw new InvalidOperationException("Connection string 'FribergHomesDB' not found.")));
+            //{
+            //    // Looking for Database FribergHomesDB
+            //    var connectionString = builder.Configuration.GetConnectionString("FribergHomesDB");
+            //    //if not found
+            //    if (connectionString == null)
+            //    {
+            //        throw new InvalidOperationException("Connection string 'FribergHomesDB' not found.");
+            //    }
+            //    // If found, the connection starts
+            //    options.UseSqlServer(connectionString);
+
+            //    // If no database called FribergHomesDB is found, it will create a Database with that name.
+            //    var dbContextOptions = new DbContextOptionsBuilder<ApplicationDBContext>().UseSqlServer(connectionString).Options;
+            //    using (var context = new ApplicationDBContext(dbContextOptions))
+            //    {
+            //        context.Database.EnsureCreated();
+            //    }
+            //});
+            
+            builder.Services.AddDbContext<ApplicationDBContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("FribergHomesDB") ?? throw new InvalidOperationException("Connection string 'FribergHomesDB' not found.")));
 
 
             // Repositories
