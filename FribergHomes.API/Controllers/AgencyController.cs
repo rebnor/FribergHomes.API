42d0f25423ec7f218bae72008de8654231db635a
sanna <sanna.nyberg@hotmail.se>
2024-04-16 16:28:20 +0200

    Added service for Agency in program.cs and did an initial DBmigration

---
+++ FribergHomes.API/Controllers/AgencyController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FribergHomes.API.Data;
+using FribergHomes.API.Models;
+
+namespace FribergHomes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgencyController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public AgencyController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Agency
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Agency>>> GetAgencies()
+        {
+            return await _context.Agencies.ToListAsync();
+        }
+
+        // GET: api/Agency/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Agency>> GetAgency(int id)
+        {
+            var agency = await _context.Agencies.FindAsync(id);
+
+            if (agency == null)
+            {
+                return NotFound();
+            }
+
+            return agency;
+        }
+
+        // PUT: api/Agency/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAgency(int id, Agency agency)
+        {
+            if (id != agency.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(agency).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AgencyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Agency
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Agency>> PostAgency(Agency agency)
+        {
+            _context.Agencies.Add(agency);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAgency", new { id = agency.Id }, agency);
+        }
+
+        // DELETE: api/Agency/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAgency(int id)
+        {
+            var agency = await _context.Agencies.FindAsync(id);
+            if (agency == null)
+            {
+                return NotFound();
+            }
+
+            _context.Agencies.Remove(agency);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AgencyExists(int id)
+        {
+            return _context.Agencies.Any(e => e.Id == id);
+        }
+    }
+}
--- FribergHomes.API/Program.cs
+++ FribergHomes.API/Program.cs
@@ -48,6 +48,7 @@ namespace FribergHomes.API
             // Repositories
             builder.Services.AddTransient<ICounty, CountyRepository>(); // Reb
             builder.Services.AddTransient<ISalesObject, SalesObjectRepository>(); // Reb
+            builder.Services.AddTransient<IAgency, AgencyRepository>(); // Sanna 
 
 
             builder.Services.AddControllers();
