0c5de41eee59ee1c3dff7c4b1067c1ede7be39f3
rebecka nordqvist <rebecka.nordqvist@hotmail.com>
2024-04-16 11:55:32 +0200

    Category Repo+Interface

---
+++ FribergHomes.API/Controllers/SalesObjectController.cs
@@ -0,0 +1,53 @@
+﻿using FribergHomes.API.Data.Interfaces;
+using FribergHomes.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FribergHomes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesObjectController : ControllerBase
+    {
+        private readonly ISalesObject _salesRepo;
+
+        public SalesObjectController(ISalesObject salesRepo)
+        {
+            _salesRepo = salesRepo;
+        }
+
+        // GET: api/<SalesObjectController>
+        [HttpGet]
+        public IActionResult GetAllSalesObjects()
+        {
+            var salesObjects = _salesRepo.GetAllSalesObjectsAsync();
+            return Ok(salesObjects);
+        }
+
+        // GET api/<SalesObjectController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SalesObjectController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SalesObjectController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SalesObjectController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FribergHomes.API/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using FribergHomes.API.Data.Interfaces;
+using FribergHomes.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FribergHomes.API.Data.Repositories
+{
+    /* Category Repository talking to ApplicationDBContext and inherit from Interface Category
+   * All in Async.
+   * @ Author: Rebecka 2024-04-16
+   */
+    public class CategoryRepository : ICategory
+    {
+        private readonly ApplicationDBContext _appDBctx;
+
+        public CategoryRepository(ApplicationDBContext appDBctx)
+        {
+            _appDBctx = appDBctx;
+        }
+        public async Task<Category> AddGategoryAsync(Category category)
+        {
+            await _appDBctx.AddAsync(category);
+            await _appDBctx.SaveChangesAsync();
+            return category;
+
+        }
+
+        public async Task DeleteGategoryAsync(Category category)
+        {
+            _appDBctx.Remove(category);
+            await _appDBctx.SaveChangesAsync();
+        }
+
+        public async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            var categories = await _appDBctx.Categories.OrderBy(c => c.Name).ToListAsync();
+            return categories;
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(int? id)
+        {
+            var category = await _appDBctx.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            return category;
+        }
+
+        public async Task<Category> UpdateGategoryAsync(Category category)
+        {
+            _appDBctx.Update(category);
+            await _appDBctx.SaveChangesAsync();
+            return category;
+        }
+    }
+}
