03a313233d986502c2890b88ed1397adf1aaa4d9
Tobias Ledin <tobias.ledin@gmail.com>
2024-04-23 17:18:02 +0200

    Changed GetSalesObjectByCounty parameter to int id (County county).

--- FribergHomes.API/Data/Interfaces/ISalesObject.cs
+++ FribergHomes.API/Data/Interfaces/ISalesObject.cs
@@ -4,11 +4,12 @@ namespace FribergHomes.API.Data.Interfaces
 {
     /* Interface for SalesObject Reopsitory
      * @ Authur: Rebecka 2024-04-15
+     * @ Update: Changed GetSalesObjectsByCountyAsync parameter to int id (County county). //Tobias 2024-04-23 
      */
     public interface ISalesObject
     {
         Task<SalesObject> GetSalesObjectByIdAsync(int? id);
-        Task<List<SalesObject>> GetSalesObjectsByCountyAsync(County county);
+        Task<List<SalesObject>> GetSalesObjectsByCountyAsync(int id);
         Task<List<SalesObject>> GetAllSalesObjectsAsync();
         Task<SalesObject> AddSalesObjectAsync(SalesObject salesObject);
         Task DeleteSalesObjectAsync(SalesObject salesObject);
