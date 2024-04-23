6fb8b848fc34a423cdedbeb2569f8db7dae18af0
Tobias Ledin <tobias.ledin@gmail.com>
2024-04-23 12:17:08 +0200

    Updated SalesObject model.

--- FribergHomes.API/Models/SalesObject.cs
+++ FribergHomes.API/Models/SalesObject.cs
@@ -7,17 +7,23 @@ namespace FribergHomes.API.Models
       * Author: Tobias 2024-04-15
       * Revised: Tobias 2024-04-17 - Flattened model structure.
       * Integrated all derived class properties into SalesObject. Added Display-names and updated required attributes.
+      * Revised: Tobias 2024-04-23 - Updated property attributes with introduction of SalesObjectDTO.
+      * Added contructor to set Listing price at object creation based on Price (for comparison reasons).
+      * Removed field values.
       */
 
     public class SalesObject
     {
+        public SalesObject()
+        {
+            ListingPrice = CurrentPrice;
+        }
+
         [Key]
         public int Id { get; set; }
 
-        [Required]
         public DateTime CreationDate { get; set; } = DateTime.Now;
 
-        [Required]
         public string CreatorName { get; set; } = string.Empty;
 
         public DateTime? ChangeDate { get; set; }
@@ -25,81 +31,49 @@ namespace FribergHomes.API.Models
         public string? ChangeName { get; set; } = string.Empty;
 
         [Required]
-        [RegularExpression(@"^[a-zA-ZåäöÅÄÖ\s-\d]+$", ErrorMessage = "Adress får enbart innehålla a-ö, 0-9, bindestreck och mellanslag")]
         public string Adress { get; set; } = string.Empty;
 
         [Required]
-        [DisplayName("Rum")]
         public int Rooms { get; set; }
 
         [Required]
-        [DisplayName("Boarea")]
         public double LivingArea { get; set; }
 
-        [DisplayName("Biarea")]
         public double AncillaryArea { get; set; }
 
-        [DisplayName("Tomtarea")]
         public double? PlotArea { get; set; }
 
-        [DisplayName("Driftkostnad")]
         public double? YearlyCost { get; set; }
 
-        [DisplayName("Månadsavgift")]
         public double? MonthlyFee { get; set; }
 
-        [DisplayName("Våning")]
         public int? Level { get; set; }
 
-        [DisplayName("Hiss")]
         public bool? Lift { get; set; }
 
         // Original price (for percent change comparison if object price changes).
         // Only gets set once when object is created.
-        [Required]
         public int ListingPrice { get; init; }
 
-
-        private int _currentPrice;
-
-        [DisplayName("Pris")]
-        public int CurrentPrice
-        { 
-            get => _currentPrice;
-            set
-            {
-                if (value != _currentPrice)
-                {
-                    _currentPrice = value;
-
-                }
-                else if (CurrentPrice == 0)
-                {
-                    _currentPrice = ListingPrice;
-                }
-            }
-        }
+        [Required]
+        public int CurrentPrice { get; set; }
 
         [Required]
-        [DisplayName("Objektbeskrivning")]
         public string ObjectDescription { get; set; } = string.Empty;
 
-        [DisplayName("Byggnadsår")]
+        [Required]
         public int BuildYear { get; set; }
 
         public List<string> ImageLinks { get; set; } = [];
 
-        [DisplayName("Visningsdatum")]
         public List<DateTime> ViewingDates { get; set; } = [];
 
-        [DisplayName("Mäklare")]
         public Realtor? Realtor { get; set; }
 
-        [DisplayName("Kommun")]
         public County? County { get; set; }
 
-        [DisplayName("Bostadskategori")]
         public Category? Category { get; set; }
+
 
     }
 }
