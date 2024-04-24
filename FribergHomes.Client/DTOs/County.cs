eb4c7e552e5870c5bb38ee0495a278f597314011
Tobias Ledin <tobias.ledin@gmail.com>
2024-04-24 10:37:32 +0200

    Created DTO-folder. Copied models from WebAPI.

---
+++ FribergHomes.Client/DTOs/County.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FribergHomes.API.Models
+{
+     /* Class for County (kommun).
+      * Author: Tobias 2024-04-15
+      */
+
+    public class County
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Kommun")]
+        [Length(3, 20, ErrorMessage = "Kommunnamnet måste bestå av minst 3 och max 20 bokstäver")]
+        [RegularExpression(@"^[a-zA-ZåäöÅÄÖ\s-]+$", ErrorMessage = "Kommunnamnet får enbart innehålla a-ö, bindestreck och mellanslag")]
+        public required string Name { get; set; }
+
+    }
+}
