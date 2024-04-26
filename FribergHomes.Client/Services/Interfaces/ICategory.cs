df9fa80f77469b4380010097de60000f8f109681
rebecka nordqvist <rebecka.nordqvist@hotmail.com>
2024-04-26 10:29:07 +0200

    CategoryService

---
+++ FribergHomes.Client/Services/Interfaces/ICategory.cs
@@ -0,0 +1,14 @@
+﻿using FribergHomes.Client.DTOs;
+
+namespace FribergHomes.Client.Services.Interfaces
+{
+    public interface ICategory
+    {
+        Task<CategoryDTO> GetCategoryByIdAsync(int? id);
+        Task<List<CategoryDTO>> GetAllCategoriesAsync();
+        Task<CategoryDTO> AddCategoryAsync(CategoryDTO category);
+        Task<CategoryDTO> UpdateCategoryAsync(CategoryDTO category);
+        Task DeleteCategoryAsync(CategoryDTO category);
+
+    }
+}
