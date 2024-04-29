82992af44be8809490869bf10ad8841b5461bf62
Tobias Ledin <tobias.ledin@gmail.com>
2024-04-29 16:16:27 +0200

    Initialized string properties to remove null warnings.

--- FribergHomes.Client/DTOs/CategoryDTO.cs
+++ FribergHomes.Client/DTOs/CategoryDTO.cs
@@ -12,8 +12,8 @@ namespace FribergHomes.Client.DTOs
         [Required]
         [Length(3, 30, ErrorMessage = "Kategorinamnet måste bestå av minst 3 och max 30 bokstäver")]
         [RegularExpression(@"^[a-zA-ZåäöÅÄÖ\s-]+$", ErrorMessage = "Kategorinamnet får enbart innehålla a-ö, bindestreck och mellanslag")]
-        public string Name { get; set; }
-        public string IconUrl { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string IconUrl { get; set; } = string.Empty;
 
     }
 }
