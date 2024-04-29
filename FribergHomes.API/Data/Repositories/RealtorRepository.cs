221b3f5f65b00f4bda8a3b9b4b989f92be0ed1d0
Tobias Ledin <tobias.ledin@gmail.com>
2024-04-29 16:14:29 +0200

    Removed joins (Include) for Realtor->SalesObject (Realtor nav prop List<SalesObject> commented out for testing.

--- FribergHomes.API/Data/Repositories/RealtorRepository.cs
+++ FribergHomes.API/Data/Repositories/RealtorRepository.cs
@@ -10,6 +10,7 @@ namespace FribergHomes.API.Data.Repositories
     // @ Update: Included Agency & Salesobjects when you Get realtor/realtors / Reb 2024-04-24
     // @ Update: Added GetAgencyByNameAsync() beasue its needed in ModelMapper / Reb 2024-04-24
     // @ Update: Added GetRealtorsSalesObjects() becuase its needed in ModelMapper / Reb 2024-04-25 <- Kanske inte alls behövs? SE ÖVER
+    // @ Update: GetAllRealtorsAsync - removed include(salesobjects). / Tobias 2024-04-29
     public class RealtorRepository : IRealtor
     {
         private readonly ApplicationDBContext _appDbCtx;
@@ -33,19 +34,19 @@ namespace FribergHomes.API.Data.Repositories
 
         public async Task<List<Realtor>> GetAllRealtorsAsync()
         {
-            var realtors = await _appDbCtx.Realtors.Include(r=>r.Agency).Include(rr=>rr.SalesObjects).OrderBy(r => r.LastName).ThenBy(r => r.FirstName).ToListAsync();
+            var realtors = await _appDbCtx.Realtors.Include(r=>r.Agency).OrderBy(r => r.LastName).ThenBy(r => r.FirstName).ToListAsync();
             return realtors;
            
         }
 
         public async Task<List<Realtor>> GetRealtorsByAgencyAsync(Agency agency)
         {
-            return await _appDbCtx.Realtors.Include(r=>r.Agency).Include(rr=>rr.SalesObjects).Where(r => r.Agency == agency).ToListAsync();
+            return await _appDbCtx.Realtors.Include(r=>r.Agency).Where(r => r.Agency == agency).ToListAsync();
         }
 
         public async Task<Realtor> GetRealtorByIdAsync(int id)
         {
-            var realtor = await _appDbCtx.Realtors.Include(r=>r.Agency).Include(rr=>rr.SalesObjects).FirstOrDefaultAsync(r => r.Id == id);
+            var realtor = await _appDbCtx.Realtors.Include(r=>r.Agency).FirstOrDefaultAsync(r => r.Id == id);
             return realtor;
         }
 
