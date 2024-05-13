68986b5ee5890ccf1dcf8ffd1853a328fef2da9d
Tobias Ledin <tobias.ledin@gmail.com>
2024-05-13 12:23:53 +0200

    Updated AuthResponseDTO: Introduced prop bool ValidCrendentials. All other props made nullable.

--- FribergHomes.API/DTOs/AuthResponseDTO.cs
+++ FribergHomes.API/DTOs/AuthResponseDTO.cs
@@ -2,8 +2,9 @@
 {
     public class AuthResponseDTO
     {
-        public string UserId { get; set; }
-        public string Token { get; set; }
-        public string Email { get; set; }
+        public string? UserId { get; set; }
+        public string? Token { get; set; }
+        public string? Email { get; set; }
+        public bool ValidCredentials { get; set; } = false;
     }
 }
