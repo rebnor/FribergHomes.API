0cbe8fe28e5bfed32303dc36189d10cab9b9f778
Tobias Ledin <tobias.ledin@gmail.com>
2024-05-13 12:21:53 +0200

    Created AuthService.

---
+++ FribergHomes.API/Services/AuthService.cs
@@ -0,0 +1,130 @@
+﻿using FribergHomes.API.Constants;
+using FribergHomes.API.Data.Interfaces;
+using FribergHomes.API.DTOs;
+using FribergHomes.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using NuGet.Common;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FribergHomes.API.Services
+{
+    // Author: Tobias 2024-05-13
+
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _config;
+        private readonly UserManager<Realtor> _userManager;
+        private readonly IRealtor _realtorRepo;
+        private readonly IAgency _agencyRepo;
+
+        public AuthService(IConfiguration config, UserManager<Realtor> userManager, IRealtor realtorRepo, IAgency agencyRepo)
+        {
+            _config = config;
+            _userManager = userManager;
+            _realtorRepo = realtorRepo;
+            _agencyRepo = agencyRepo;
+        }
+
+        /// <summary>
+        /// Realtor login method
+        /// </summary>
+        /// <param name="credentials"></param>
+        /// <returns></returns>
+        public async Task<AuthResponseDTO> Login(LoginRealtorDTO credentials)
+        {
+            var realtor = await _userManager.FindByEmailAsync(credentials.Email);
+            var passwordValid = await _userManager.CheckPasswordAsync(realtor, credentials.Password);
+
+            var response = new AuthResponseDTO();
+
+            if (realtor != null && passwordValid)
+            {
+                response.Email = credentials.Email;
+                response.Token = await GenerateToken(realtor);
+                response.UserId = realtor!.Id;
+                response.ValidCredentials = true;
+            }
+
+            return response;
+        }
+
+        public async Task<IdentityResult> Register(RegisterRealtorDTO realtorData)
+        {
+            
+            
+                //TODO: Uppdatera AutoMapper + resolver för Realtor, implementera sedan här! / Tobias
+                Realtor realtor = new()
+                {
+                    UserName = realtorData.Email,
+                    Email = realtorData.Email,
+                    FirstName = realtorData.FirstName,
+                    LastName = realtorData.LastName,
+                    PhoneNumber = realtorData.PhoneNumber,
+                    Agency = await _agencyRepo.GetAgencyByIdAsync(realtorData.AgencyId)
+                };
+
+                var result = await _userManager.CreateAsync(realtor, realtorData.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(realtor, ApiRoles.Realtor);
+                }
+
+                return result;
+
+            
+            
+
+        }
+
+        public async Task RenewToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ValidateToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Realtor> GetRealtorFromToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private async Task<string> GenerateToken(Realtor realtor)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var roles = await _userManager.GetRolesAsync(realtor);
+            var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+
+            var userClaims = await _userManager.GetClaimsAsync(realtor);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, realtor.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, realtor.Email),
+                new Claim(CustomClaimTypes.Uid, realtor.Id)
+            }
+            .Union(roleClaims)
+            .Union(userClaims);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JwtSettings:Issuer"],
+                audience: _config["JwtSettings:audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(_config["JwtSettings:DurationInMinutes"]))
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ FribergHomes.API/Services/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using FribergHomes.API.DTOs;
+using FribergHomes.API.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace FribergHomes.API.Services
+{
+    public interface IAuthService
+    {
+
+        Task<AuthResponseDTO> Login(LoginRealtorDTO credentials);
+
+        Task<IdentityResult> Register(RegisterRealtorDTO realtor);
+
+        Task ValidateToken(string token);
+
+        Task RenewToken(string token);
+
+        Task<Realtor> GetRealtorFromToken(string token);
+    }
+}
