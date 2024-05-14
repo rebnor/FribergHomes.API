c32186293b2a61843e0a8e6b6e9dac4c072da745
Tobias Ledin <tobias.ledin@gmail.com>
2024-05-14 14:38:09 +0200

    Removed unused UserName prop.

--- FribergHomes.API/DTOs/RegisterRealtorDTO.cs
+++ FribergHomes.API/DTOs/RegisterRealtorDTO.cs
@@ -1,13 +1,29 @@
-﻿namespace FribergHomes.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace FribergHomes.API.DTOs
 {
     public class RegisterRealtorDTO
     {
-        //public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [PasswordPropertyText]
         public string Password { get; set; }
+
+        [Required]
         public int AgencyId { get; set; }
     }
 }
