a96eff4a66c89235982301aedb93482a39407f20
Tobias Ledin <tobias.ledin@gmail.com>
2024-05-15 16:17:45 +0200

    Added AuthService method GetUserId.

--- FribergHomes.Client/Authentications/AuthService.cs
+++ FribergHomes.Client/Authentications/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using Blazored.LocalStorage;
+using FribergHomes.Client.Constants;
 using FribergHomes.Client.DTOs;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Net.Http.Headers;
@@ -87,6 +88,29 @@ namespace FribergHomes.Client.Authentications
         public void ConfigureHttpClientWithToken(HttpClient httpClient, string token)
         {
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        /// Author: Tobias 2024-05-15
+        /// <summary>
+        /// Method to get Id of currently logged in user.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetUserId()
+        {
+            if (_authStateProvider is not null)
+            {
+                var authState = await _authStateProvider.GetAuthenticationStateAsync();
+                var userIdClaim = authState.User.Claims.FirstOrDefault(x => x.Type == CustomClaimTypes.Uid);
+
+                if (userIdClaim is not null)
+                {
+                    var userId = userIdClaim.Value;
+
+                    return userId;
+                }
+            }
+
+            return string.Empty;
         }
 
     }
