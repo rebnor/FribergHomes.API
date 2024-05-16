545682cc8a1b32f6f3f0d172a81fa86876120123
rebecka nordqvist <rebecka.nordqvist@hotmail.com>
2024-05-16 11:00:51 +0200

    kolalr token datum

--- FribergHomes.Client/Authentications/AuthProvider.cs
+++ FribergHomes.Client/Authentications/AuthProvider.cs
@@ -17,17 +17,24 @@ namespace FribergHomes.Client.Authentications
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            var user = new ClaimsPrincipal(new ClaimsIdentity()); // gör en ny tom användare
+
             var jwt = await _localStorage.GetItemAsync<string>("jwt"); // hämtar token som ligger i localStorage jwt
             // Om jwt är tom eller null returneras ett AuthenticationState med tomt ClaimsPrinciple.// Tobias
             if (string.IsNullOrEmpty(jwt))
             {
                 return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())); 
             }
+            
             var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
             var jwtContent = jwtSecurityTokenHandler.ReadJwtToken(jwt); // Hämtar token innehåll
+            if (jwtContent.ValidTo < DateTime.Now) // Ser över om tiden har gått ut på token
+            {
+                return new AuthenticationState(user); // Skickar ny state med tomma användaren
+            }
             var claims = jwtContent.Claims.ToList(); // hämtar claims i token
             claims.Add(new Claim(ClaimTypes.Name, jwtContent.Subject));
-            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt")); // hämtar användare med denna token
+            user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt")); // uppdaterar användare med denna token
             var authState = Task.FromResult(new AuthenticationState(user)); // Sätter user i ny authentication state
             NotifyAuthenticationStateChanged(authState); // megafon om att state är ändrad
             return authState.Result; // skickar state svar
